61268e060bbe2429f2e9a6e2c7d468342e287901
Vitaly <al-not@ya.ru>
2019-02-28 00:30:21 +0500

    add test button

--- app/src/main/java/online/z0lk1n/android/handnotes/MainActivity.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/MainActivity.kt
@@ -1,12 +1,14 @@
 package online.z0lk1n.android.handnotes
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_test.setOnClickListener { tv_test.text = "Good work!" }
     }
 }
