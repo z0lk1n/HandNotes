269df1c114a9312b9517244ddf596655c5eb8d86
Vitaly <al-not@ya.ru>
2019-03-03 13:31:27 +0500

    deleted test view

--- app/src/main/java/online/z0lk1n/android/handnotes/MainActivity.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/MainActivity.kt
@@ -2,13 +2,11 @@ package online.z0lk1n.android.handnotes
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_test.setOnClickListener { tv_test.text = "Good work!" }
     }
 }
