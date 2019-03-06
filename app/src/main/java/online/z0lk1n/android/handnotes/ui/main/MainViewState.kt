ae74fcf2a3cc7e301f74c39e2a51af74812d9ee8
Vitaly <al-not@ya.ru>
2019-03-06 23:15:35 +0500

    added observe MainViewModel for repository

--- app/src/main/java/online/z0lk1n/android/handnotes/ui/main/MainViewState.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/ui/main/MainViewState.kt
@@ -2,4 +2,4 @@ package online.z0lk1n.android.handnotes.ui.main
 
 import online.z0lk1n.android.handnotes.data.entity.Note
 
-class MainViewState(val notes: List<Note>)
+data class MainViewState(val notes: List<Note>)
