2275e124dec4d6b84767e18cff845590320e217a
Vitaly <al-not@ya.ru>
2019-03-06 22:53:30 +0500

    added colors in Note and implementation Parcelable in Note

--- app/src/main/java/online/z0lk1n/android/handnotes/data/entity/Note.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/data/entity/Note.kt
@@ -1,3 +1,61 @@
 package online.z0lk1n.android.handnotes.data.entity
 
-class Note(val title: String, val text: String, val color: Int = 0xFFFFFFFF.toInt())
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.*
+
+class Note(
+    val id: String,
+    val title: String,
+    val text: String,
+    val color: Color = Color.WHITE,
+    val lastChanged: Date = Date()
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        enumValueOf<Color>(parcel.readString()!!),
+        Date(parcel.readLong())
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(title)
+        parcel.writeString(text)
+        parcel.writeString(color.name)
+        parcel.writeLong(lastChanged.time)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Note> {
+
+        override fun createFromParcel(parcel: Parcel): Note = Note(parcel)
+
+        override fun newArray(size: Int): Array<Note?> = arrayOfNulls(size)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Note
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode() = id.hashCode()
+
+    enum class Color {
+        WHITE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        VIOLET,
+        PINK
+    }
+}
