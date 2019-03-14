5757163583904a0eccfb16315f0f26d12564381c
Vitaly <al-not@ya.ru>
2019-03-14 19:34:32 +0500

    rebuilt Main screen

--- app/src/main/java/online/z0lk1n/android/handnotes/ui/main/MainViewModel.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/ui/main/MainViewModel.kt
@@ -1,20 +1,35 @@
 package online.z0lk1n.android.handnotes.ui.main
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.Observer
 import online.z0lk1n.android.handnotes.data.NotesRepository
+import online.z0lk1n.android.handnotes.data.entity.Note
+import online.z0lk1n.android.handnotes.model.NoteResult
+import online.z0lk1n.android.handnotes.ui.base.BaseViewModel
 
-class MainViewModel(private val repository: NotesRepository = NotesRepository) : ViewModel() {
+class MainViewModel(private val repository: NotesRepository = NotesRepository) :
+    BaseViewModel<List<Note>?, MainViewState>() {
 
-    private val viewStateLiveData: MutableLiveData<MainViewState> = MutableLiveData()
+    private val notesObserver = Observer<NoteResult> {
+        it ?: let { return@Observer }
 
-    init {
-        repository.getNotes().observeForever { notes ->
-            viewStateLiveData.value =
-                viewStateLiveData.value?.copy(notes = notes!!) ?: MainViewState(notes!!)
+        when (it) {
+            is NoteResult.Success<*> -> {
+                viewStateLiveData.value = MainViewState(it.data as? List<Note>)
+            }
+            is NoteResult.Error -> {
+                viewStateLiveData.value = MainViewState(error = it.error)
+            }
         }
     }
 
-    fun viewState(): LiveData<MainViewState> = viewStateLiveData
+    private val repositoryNotes = repository.getNotes()
+
+    init {
+        viewStateLiveData.value = MainViewState()
+        repositoryNotes.observeForever(notesObserver)
+    }
+
+    override fun onCleared() {
+        repositoryNotes.removeObserver(notesObserver)
+    }
 }
--- app/src/main/java/online/z0lk1n/android/handnotes/ui/main/MainViewState.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/ui/main/MainViewState.kt
@@ -1,5 +1,6 @@
 package online.z0lk1n.android.handnotes.ui.main
 
 import online.z0lk1n.android.handnotes.data.entity.Note
+import online.z0lk1n.android.handnotes.ui.base.BaseViewState
 
-data class MainViewState(val notes: List<Note>)
+class MainViewState(notes: List<Note>? = null, error: Throwable? = null) : BaseViewState<List<Note>?>(notes, error)
