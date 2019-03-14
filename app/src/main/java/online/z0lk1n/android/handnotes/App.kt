cb1a3958bef493d718b7959c48caff4157656b10
Vitaly <al-not@ya.ru>
2019-03-14 12:13:51 +0500

    set up Firebase firestore

---
+++ app/src/main/java/online/z0lk1n/android/handnotes/App.kt
@@ -0,0 +1,9 @@
+package online.z0lk1n.android.handnotes
+
+import android.support.multidex.MultiDexApplication
+
+class App : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
