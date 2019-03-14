21e7ce4a8b21bad1f217d47126e1e9d6d3bc9ba5
Vitaly <al-not@ya.ru>
2019-03-14 16:46:50 +0500

    rebuilt note repository

--- app/src/main/java/online/z0lk1n/android/handnotes/data/NotesRepository.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/data/NotesRepository.kt
@@ -1,70 +1,14 @@
 package online.z0lk1n.android.handnotes.data
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import online.z0lk1n.android.handnotes.data.entity.Note
-import java.util.*
+import online.z0lk1n.android.handnotes.data.provider.FireStoreProvider
+import online.z0lk1n.android.handnotes.data.provider.RemoteDataProvider
 
 object NotesRepository {
 
-    private val notesLiveData = MutableLiveData<List<Note>>()
+    private val remoteDataProvider: RemoteDataProvider = FireStoreProvider()
 
-    val notes: MutableList<Note> = mutableListOf(
-        Note(
-            UUID.randomUUID().toString(),
-            "Первая заметка",
-            "Текст первой заметки. Не очень длинный, но интересный"
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Вторая заметка",
-            "Текст второй заметки. Не очень длинный, но интересный",
-            Note.Color.YELLOW
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Третья заметка",
-            "Текст третьей заметки. Не очень длинный, но интересный",
-            Note.Color.GREEN
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Четвертая заметка",
-            "Текст четвертой заметки. Не очень длинный, но интересный",
-            Note.Color.BLUE
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Пятая заметка",
-            "Текст пятой заметки. Не очень длинный, но интересный",
-            Note.Color.RED
-        ),
-        Note(
-            UUID.randomUUID().toString(),
-            "Шестая заметка",
-            "Текст шестой заметки. Не очень длинный, но интересный",
-            Note.Color.VIOLET
-        )
-    )
-
-    init {
-        notesLiveData.value = notes
-    }
-
-    fun getNotes(): LiveData<List<Note>> = notesLiveData
-
-    fun saveNote(note: Note) {
-        addOrReplace(note)
-        notesLiveData.value = notes
-    }
-
-    private fun addOrReplace(note: Note) {
-        for (i in 0 until notes.size) {
-            if (notes[i] == note) {
-                notes[i] = note
-                return
-            }
-        }
-        notes.add(note)
-    }
+    fun getNoteById(id: String) = remoteDataProvider.getNoteById(id)
+    fun saveNote(note: Note) = remoteDataProvider.saveNote(note)
+    fun getNotes() = remoteDataProvider.subscribeToAllNotes()
 }
