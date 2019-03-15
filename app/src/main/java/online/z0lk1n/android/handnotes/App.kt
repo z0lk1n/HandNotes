b97cf9531e2e93dfaa3b2a69e5fda4745f205a57
Vitaly <al-not@ya.ru>
2019-03-15 21:22:22 +0500

    added Timberkt in application

--- app/src/main/java/online/z0lk1n/android/handnotes/App.kt
+++ app/src/main/java/online/z0lk1n/android/handnotes/App.kt
@@ -1,9 +1,13 @@
 package online.z0lk1n.android.handnotes
 
 import android.app.Application
+import com.github.ajalt.timberkt.Timber
 
 class App : Application() {
+
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(timber.log.Timber.DebugTree())
     }
 }
