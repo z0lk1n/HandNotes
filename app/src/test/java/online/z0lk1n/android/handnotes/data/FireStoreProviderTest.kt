23a2ad9398fe7776f558e4ab06dfd4a88d3a4924
Vitaly <al-not@ya.ru>
2019-03-28 00:43:50 +0500

    added FireStoreProviderTest

---
+++ app/src/test/java/online/z0lk1n/android/handnotes/data/FireStoreProviderTest.kt
@@ -0,0 +1,124 @@
+package online.z0lk1n.android.handnotes.data
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.*
+import io.mockk.*
+import junit.framework.Assert
+import online.z0lk1n.android.handnotes.data.entity.Note
+import online.z0lk1n.android.handnotes.data.errors.NoAuthException
+import online.z0lk1n.android.handnotes.data.provider.FireStoreProvider
+import online.z0lk1n.android.handnotes.model.NoteResult
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class FireStoreProviderTest {
+
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
+    private val mockDb = mockk<FirebaseFirestore>()
+    private val mockAuth = mockk<FirebaseAuth>()
+    private val mockCollection = mockk<CollectionReference>()
+    private val mockUser = mockk<FirebaseUser>()
+
+    private val mockDocument1 = mockk<DocumentSnapshot>()
+    private val mockDocument2 = mockk<DocumentSnapshot>()
+    private val mockDocument3 = mockk<DocumentSnapshot>()
+
+    private val testNotes = listOf(Note(id = "1"), Note(id = "2"), Note(id = "3"))
+
+    private val provider: FireStoreProvider = FireStoreProvider(mockAuth, mockDb)
+
+
+    @Before
+    fun setup() {
+        clearMocks(mockCollection, mockDocument1, mockDocument2, mockDocument3)
+
+        every { mockAuth.currentUser } returns mockUser
+        every { mockUser.uid } returns ""
+        every { mockDb.collection(any()).document(any()).collection(any()) } returns mockCollection
+
+        every { mockDocument1.toObject(Note::class.java) } returns testNotes[0]
+        every { mockDocument2.toObject(Note::class.java) } returns testNotes[1]
+        every { mockDocument3.toObject(Note::class.java) } returns testNotes[2]
+    }
+
+    @Test
+    fun `should throw NoAuthException if no auth`() {
+        var result: Any? = null
+        every { mockAuth.currentUser } returns null
+        provider.subscribeToAllNotes().observeForever {
+            result = (it as? NoteResult.Error)?.error
+        }
+        Assert.assertTrue(result is NoAuthException)
+    }
+
+    @Test
+    fun `subscribeToAllNotes returns notes`() {
+        var result: List<Note>? = null
+        val slot = slot<EventListener<QuerySnapshot>>()
+
+        val mockSnapshot = mockk<QuerySnapshot>()
+
+        every { mockSnapshot.documents } returns listOf(mockDocument1, mockDocument2, mockDocument3)
+        every { mockCollection.addSnapshotListener(capture(slot)) } returns mockk()
+
+        provider.subscribeToAllNotes().observeForever {
+            result = (it as? NoteResult.Success<List<Note>>)?.data
+        }
+        slot.captured.onEvent(mockSnapshot, null)
+        Assert.assertEquals(testNotes, result)
+    }
+
+    @Test
+    fun `subscribeAllNotes return error`() {
+        var result: Throwable? = null
+        val slot = slot<EventListener<QuerySnapshot>>()
+        val testError = mockk<FirebaseFirestoreException>()
+
+        every { mockCollection.addSnapshotListener(capture(slot)) } returns mockk()
+
+        provider.subscribeToAllNotes().observeForever {
+            result = (it as? NoteResult.Error)?.error
+        }
+        slot.captured.onEvent(null, testError)
+
+        Assert.assertNotNull(result)
+        Assert.assertEquals(testError, result)
+    }
+
+    @Test
+    fun `saveNote calls document set`() {
+        val mockDocumentReference = mockk<DocumentReference>()
+        every { mockCollection.document(testNotes[0].id) } returns mockDocumentReference
+        provider.saveNote(testNotes[0])
+        verify(exactly = 1) { mockDocumentReference.set(testNotes[0]) }
+    }
+
+    @Test
+    fun `saveNote returns note`() {
+        val mockDocumentReference: DocumentReference = mockk()
+        val slot = slot<OnSuccessListener<in Void>>()
+        var result: Note? = null
+
+        every { mockCollection.document(testNotes[0].id) } returns mockDocumentReference
+        every {
+            mockDocumentReference.set(testNotes[0]).addOnSuccessListener(capture(slot))
+        }
+
+        provider.saveNote(testNotes[0]).observeForever {
+            result = (it as? NoteResult.Success<Note>)?.data
+        }
+
+        slot.captured.onSuccess(null)
+
+        Assert.assertNotNull(result)
+        Assert.assertEquals(testNotes[0], result)
+    }
+
+    //TODO: tests for delete
+}
