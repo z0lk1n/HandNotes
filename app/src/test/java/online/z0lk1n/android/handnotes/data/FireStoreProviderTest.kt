0ad250a7fd286df46302283aa4976f9e4e460e37
Vitaly <al-not@ya.ru>
2019-03-28 19:44:52 +0500

    added tests deleteNote

--- app/src/test/java/online/z0lk1n/android/handnotes/data/FireStoreProviderTest.kt
+++ app/src/test/java/online/z0lk1n/android/handnotes/data/FireStoreProviderTest.kt
@@ -1,6 +1,7 @@
 package online.z0lk1n.android.handnotes.data
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -11,6 +12,7 @@ import online.z0lk1n.android.handnotes.data.entity.Note
 import online.z0lk1n.android.handnotes.data.errors.NoAuthException
 import online.z0lk1n.android.handnotes.data.provider.FireStoreProvider
 import online.z0lk1n.android.handnotes.model.NoteResult
+import online.z0lk1n.android.handnotes.ui.note.NoteViewState
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -70,6 +72,8 @@ class FireStoreProviderTest {
             result = (it as? NoteResult.Success<List<Note>>)?.data
         }
         slot.captured.onEvent(mockSnapshot, null)
+
+        assertNotNull(result)
         assertEquals(testNotes, result)
     }
 
@@ -119,5 +123,60 @@ class FireStoreProviderTest {
         assertEquals(testNotes[0], result)
     }
 
-    //TODO: tests for delete
+    @Test
+    fun `saveNote returns error`() {
+        val mockDocumentReference: DocumentReference = mockk()
+        val testError = mockk<FirebaseFirestoreException>()
+        val slot = slot<OnFailureListener>()
+        var result: Throwable? = null
+
+        every { mockCollection.document(testNotes[0].id) } returns mockDocumentReference
+        every { mockDocumentReference.set(testNotes[0]).addOnFailureListener(capture(slot)) }
+
+        provider.saveNote(testNotes[0]).observeForever {
+            result = (it as? NoteResult.Error)?.error
+        }
+
+        slot.captured.onFailure(testError)
+
+        assertNotNull(result)
+        assertEquals(testError, result)
+    }
+
+    @Test
+    fun `deleteNote returns success`()   {
+        var result: NoteViewState.Data? = null
+        val mockDocumentReference: DocumentReference = mockk()
+        val slot = slot<OnSuccessListener<in Void>>()
+
+        every { mockCollection.document(testNotes[0].id) } returns mockDocumentReference
+        every { mockDocumentReference.delete().addOnSuccessListener(capture(slot)) }
+
+        provider.deleteNote(testNotes[0].id).observeForever {
+            result = (it as? NoteResult.Success<NoteViewState.Data>)?.data
+        }
+        slot.captured.onSuccess(null)
+
+        assertNotNull(result)
+        assertEquals(testNotes[0], result)
+    }
+
+    @Test
+    fun `deleteNote returns error`()   {
+        var result: Throwable? = null
+        val mockDocumentReference: DocumentReference = mockk()
+        val slot = slot<OnFailureListener>()
+        val testError = mockk<FirebaseFirestoreException>()
+
+        every { mockCollection.document(testNotes[0].id) } returns mockDocumentReference
+        every { mockDocumentReference.delete().addOnFailureListener(capture(slot)) }
+
+        provider.deleteNote(testNotes[0].id).observeForever {
+            result = (it as? NoteResult.Error)?.error
+        }
+        slot.captured.onFailure(testError)
+
+        assertNotNull(result)
+        assertEquals(testError, result)
+    }
 }
